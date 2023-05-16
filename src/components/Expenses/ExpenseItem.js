fc7685f9600ad8df4ffdb1e8bf871313f2119644
pyojeyn <jeynpyo@gmail.com>
2023-05-16 16:50:04 +0900

    react start !

---
+++ src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,21 @@
+// 리액트에 있는 컴포넌트는 단지 자바스크립트 함수일 뿐!
+import './ExpenseItem.css';
+import ExpenseDate from './ExpenseDate';
+import Card from '../UI/Card';
+
+// 리액트에서 class 속성은 className!
+
+const ExpenseItem = props => {
+    
+    return (
+    <Card className="expense-item">
+        <ExpenseDate date={props.date}/>
+        <div className='expense-item__description'>
+            <h2>{props.title}</h2>
+            <div className='expense-item__price'>${props.amount}</div>
+        </div>
+    </Card>
+    );
+} 
+
+export default ExpenseItem;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './App'; // 파일 이름은 항상 .js를 빼고 입력. 
 
+// 페이지가 로드되면 가장 먼저 실행되는 파일. 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App /> 
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
