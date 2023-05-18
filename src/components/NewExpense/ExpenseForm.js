151a626a02c7d35e857bf0592a8456c5ba108763
pyojeyn <jeynpyo@gmail.com>
2023-05-18 18:32:24 +0900

    1. 이전 state 에 의존하는 State 업데이트 2. form 제출 처리 3. 양방향 바인딩 4. 자식 -> 부모 컴포넌트 통신 (상향식)

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,27 +1,75 @@
-import React from "react";
+import React, { useState } from "react";
 import './ExpenseForm.css';
 
 
-const ExpenseForm = () =>{
-    const titleChangeHandler = (event) => {
-        console.log(event.target.value);
+const ExpenseForm = (props) =>{
+   const [enteredTitle, setEnteredTitle] = useState('');
+   const [enteredAmount, setEnteredAmount] = useState('');
+   const [enteredDate, setEnteredDate] = useState('');
+
+    
+
+    const titleChangeHandler = event => {
+        setEnteredTitle(event.target.value);
+        // setUserInput({
+        //     ...userInput, // enteredTitle 말고 다른 key들도 유지하기 위해. 
+        //     enteredTitle: event.target.value, // 입력된 title 을 오버라이드 
+        // })
+
+        // setUserInput((prevState) => {
+        //     return { ...prevState, enteredTitle: event.target.value };
+        // })
     };
 
+    const amountChangeHandler = event => {
+        setEnteredAmount(event.target.value);
+        // setUserInput({
+        //     ...userInput,
+        //     enteredAmount: event.target.value
+        // })
+    };
+
+    const dateChangeHandler = event => {
+        setEnteredDate(event.target.value);
+        // setUserInput({
+        //     ...userInput,
+        //     enteredDate: event.target.value
+        // })
+    };
+
+    const submitHandler = (event) => {
+        
+        event.preventDefault();
+        const expenseData = {
+            title: enteredTitle,
+            amount: enteredAmount,
+            date: new Date(enteredDate)
+        }
+
+        props.onSaveExpenseData(expenseData);
+        setEnteredAmount('');
+        setEnteredDate('');
+        setEnteredTitle('');
+    };
 
     return (
-    <form>
+    <form onSubmit={submitHandler}>
         <div className="new-expense__controls">
             <div className="new-expense__control">
                 <label>Title</label>
-                <input type="text" onChange={titleChangeHandler}/>
+                <input type="text" value={enteredTitle} onChange={titleChangeHandler}/>
             </div>
             <div className="new-expense__control">
                 <label>Amount</label>
-                <input type="number" min="0.01" step="0.01"/>
+                <input type="number" min="0.01" step="0.01" 
+                onChange={amountChangeHandler} 
+                value={enteredAmount}/>
             </div>
             <div className="new-expense__control">
                 <label>Date</label>
-                <input type="date" min="2019-01-01" max="2023-12-31"/>
+                <input type="date" min="2019-01-01" 
+                max="2023-12-31" onChange={dateChangeHandler}
+                value={enteredDate}/>
             </div>
         </div>
         <div className="new-expense__actions">
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -2,10 +2,20 @@ import React from "react";
 import './NewExpenses.css';
 import ExpenseForm from "./ExpenseForm";
 
-const NewExpense = () => {
+const NewExpense = (props) => {
+    // saveExpenseDataHandler: 심부름꾼임 야 ExpenseForm 가서 데이터 받아와
+    // enteredExpenseData : 여기다 담아가지고 와. 
+    const saveExpenseDataHandler = (enteredExpenseData) => {
+        const expenseData = {
+            ...enteredExpenseData,
+            id: Math.random().toString()
+        };
+        props.onAddExpense(expenseData);
+    }
+
     return ( 
     <div className="new-expense">
-        <ExpenseForm />
+        <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
     </div>
     );
 };
