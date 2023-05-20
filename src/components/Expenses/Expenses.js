06b90945abba49c2ffa7d527d78c979585248992
pyojeyn <jeynpyo@gmail.com>
2023-05-20 16:28:25 +0900

    filter() 를 이용해서 조건에 따라 출력하기.

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -12,6 +12,10 @@ const Expenses = props => {
         setFilteredYear(selectedYear);
     }
 
+    const filteredExpense = props.items.filter(expense =>{
+        return expense.date.getFullYear().toString() === filteredYear;
+    });
+
     return(
         <div>
             <Card className="expenses">
@@ -19,7 +23,7 @@ const Expenses = props => {
             selected={filteredYear}
             onChangeFilter={filterChangeHandler}/>
             {
-                props.items.map((item) => (
+                filteredExpense.map((item) => (
                     <ExpenseItem
                         key = {item.id}
                         title={item.title}
