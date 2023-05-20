5bf53cf673e80ce3a787a93400f95ae0a502a124
pyojeyn <jeynpyo@gmail.com>
2023-05-20 15:31:59 +0900

    동적으로 배열 렌더링하기!

--- src/App.js
+++ src/App.js
@@ -1,35 +1,38 @@
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
+import React, {useState} from "react";
+
+const DUMMY_EXPENSE = [
+  {
+    id: 'e1',
+    title: 'Toilet Paper',
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: 'e3',
+    title: 'Car Insurance',
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: 'e4',
+    title: 'New Desk (Wooden)',
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
+
 
 
 // JSX 자바스크립트 안에 있는 HTML 코드 , 자바스크립트 XML 
 const App =() => {
-  const expenses = [
-    {
-      id: 'e1',
-      title: 'Toilet Paper',
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
-    {
-      id: 'e3',
-      title: 'Car Insurance',
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: 'e4',
-      title: 'New Desk (Wooden)',
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-  ];
-
+  const[expenses, setExpenses] = useState(DUMMY_EXPENSE);
   const addExpenseHandler = expense => {
-    console.log('In App.js');
-    console.log(expense);
-
+    setExpenses((prevExpenses => {
+      return [expense, ...prevExpenses]
+    }));
   }
 
   return (
