3da62eea2169a08d1301bb1c4c1eda5ac5b57203
pyojeyn <jeynpyo@gmail.com>
2023-05-20 11:46:33 +0900

    이벤트 및 state 작업하기

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -7,23 +7,13 @@ import React, { useState } from 'react';
 // 리액트에서 class 속성은 className!
 
 const ExpenseItem = props => {
-    
-    const [title, setTitle] = useState(props.title); //  let title = props.title 랑 똑같은 역할. 
-    // const [현재 상태값, 업데이트하는 합수] = useState(초기값);
-    const clickHandler = () => { 
-        setTitle('Updated!');
-        console.log(title);
-    }
-
-
     return (
     <Card className="expense-item">
         <ExpenseDate date={props.date}/>
         <div className='expense-item__description'>
-            <h2>{title}</h2>
+            <h2>{props.title}</h2>
             <div className='expense-item__price'>${props.amount}</div>
         </div>
-        <button onClick={clickHandler}>Change Title</button>
     </Card>
     );
 } 
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,20 +1,34 @@
+import React, { useState } from "react";
+
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import './Expenses.css';
+import ExpensesFilter from "./ExpensesFilter";
 
 const Expenses = props => {
+    const[filteredYear, setFilteredYear] = useState('2020');
+
+    const filterChangeHandler = selectedYear => {
+        setFilteredYear(selectedYear);
+    }
+
     return(
-        <Card className="expenses">
-        {
-            props.items.map((item) => (
-                <ExpenseItem
-                    key = {item.id}
-                    title={item.title}
-                    amount={item.amount}
-                    date={item.date}/>
-            ))
-        }
-        </Card>
+        <div>
+            <Card className="expenses">
+            <ExpensesFilter 
+            selected={filteredYear}
+            onChangeFilter={filterChangeHandler}/>
+            {
+                props.items.map((item) => (
+                    <ExpenseItem
+                        key = {item.id}
+                        title={item.title}
+                        amount={item.amount}
+                        date={item.date}/>
+                ))
+            }
+            </Card>
+        </div>
     )
 }
 
