10238f19cf906d831145f46d7255ad586b963d7b
pyojeyn <jeynpyo@gmail.com>
2023-05-20 17:09:39 +0900

    조건에 따라 컴포넌트 조작하기2

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import './NewExpenses.css';
 import ExpenseForm from "./ExpenseForm";
 
 const NewExpense = (props) => {
+    const[isEditing, setIsEditing] = useState(false);
+
+
     // saveExpenseDataHandler: 심부름꾼임 야 ExpenseForm 가서 데이터 받아와
     // enteredExpenseData : 여기다 담아가지고 와. 
     const saveExpenseDataHandler = (enteredExpenseData) => {
@@ -11,11 +14,21 @@ const NewExpense = (props) => {
             id: Math.random().toString()
         };
         props.onAddExpense(expenseData);
+        setIsEditing(false);
+    }
+
+    const startEditingHandler = () =>{
+        setIsEditing(true);
+    }
+
+    const stopEditingHandler = () => {
+        setIsEditing(false);
     }
 
     return ( 
     <div className="new-expense">
-        <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+        {!isEditing && <button onClick={startEditingHandler}>Add New Expense</button>}
+        {isEditing && <ExpenseForm onSaveExpenseData={saveExpenseDataHandler}  onCancel={stopEditingHandler}/>}
     </div>
     );
 };
