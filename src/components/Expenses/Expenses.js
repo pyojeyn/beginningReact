f0bb4cde469f1636e1319269c4647683cb9c648a
pyojeyn <jeynpyo@gmail.com>
2023-05-21 11:37:12 +0900

    차트 추가

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -4,6 +4,7 @@ import Card from "../UI/Card";
 import './Expenses.css';
 import ExpensesFilter from "./ExpensesFilter";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 
 const Expenses = props => {
     const[filteredYear, setFilteredYear] = useState('2020');
@@ -23,6 +24,7 @@ const Expenses = props => {
             <ExpensesFilter 
             selected={filteredYear}
             onChangeFilter={filterChangeHandler}/>
+            <ExpensesChart  expenses={filteredExpense}/>
             <ExpensesList items={filteredExpense}/>
             </Card>
         </div>
