fd842e1618be0c792545f2f158f498b91d2d252a
pyojeyn <jeynpyo@gmail.com>
2023-05-21 11:40:40 +0900

    ExpenseList 추가

--- src/components/Expenses/ExpensesList.js
+++ src/components/Expenses/ExpensesList.js
@@ -4,8 +4,6 @@ import './ExpensesList.css';
 
 const ExpensesList = props => {
 
-    let expensesContent = <p>No expense found.</p>;
-
     if(props.items.length === 0){
         return <h2 className="expenses-list__fallback">Found no expenses</h2>
     };
